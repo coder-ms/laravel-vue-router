64ddd5f11addfba29344f2475bf58db16293e9a6
coder-ms <marco.sbrascia@gmail.com>
2023-01-18 18:49:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>{{ title }}</h1>
+  <ul>
+    <li v-for="post in posts">
+      {{ post.title }}
+    </li>
+  </ul>
+</template>
+
+
+<script>
+import axios from 'axios';
+export default {
+  name: "App",
+  data(){
+    return {
+      title: "hello world",
+      apiURL: "http://localhost:8000/api",
+      posts: [],
+    }
+  },
+  methods: {
+    getPosts(){
+      axios.get(`${this.apiURL}/posts`).then((response)=>{
+        console.log(response);
+        this.posts=response.data.results.data;
+        console.log(this.posts);
+        console.log(response.data.results);
+      });
+    }
+  },
+
+  mounted(){
+    this.getPosts();
+  },
+};
+</script>
+
+<style>
+
+</style>
