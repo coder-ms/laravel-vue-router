0277dbae42adb8908e53261cf58803ca3136c23e
coder-ms <marco.sbrascia@gmail.com>
2023-01-19 17:13:02 +0200

    base send form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <!-- <div class="row">
+        <div class="col-12">
+        <h1 class="mt-5 mb-3">Contatti</h1>
+        
+        </div>
+    </div> -->
+    <div>
+        ContactForm
+    </div>
+
+  </template>
+    
+    
+<script>
+    import axios from 'axios';
+    import { store } from '../store';
+     
+    export default {
+      name: 'ContactForm',
+      data(){
+        return{
+            store,
+            name: '',
+            email: '', 
+            message: '',
+            success: false,
+            errors: {}
+        }
+      },
+      methods:{
+        sendForm(){
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            }
+            axios.post(`${this.store.apiBaseUrl}/contacts`, data).then((response)=>{
+                console.log(response.data);
+            });
+        }
+      }
+    }
+    </script>
+    
+    <style lang="scss" scoped>
+    
+    </style>
--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,18 +1,25 @@
 <template>
-    <div>
-        ContactUs
+  <section>
+    <div class="row">
+        <div class="col-12">
+          <h1 class="mt-5 mb-3">Contatti</h1>
+          <ContactForm />
+        </div>
     </div>
-   </template>
-     
-     
-   <script>
-   
-     export default {
-       name: 'ContactUs',
-   
-     }
-     </script>
-     
-     <style lang="scss" scoped>
-     
-     </style>
+  </section>
+</template>
+  
+  
+<script>
+import ContactForm from '../components/ContactForm.vue'
+  export default {
+    name: 'ContactUs',
+    components:{
+            ContactForm
+        }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
